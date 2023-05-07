b8aea66696ca94800e8315e6a7400c86c41845e0
AbrarJahin0007 <132800840+AbrarJahin0007@users.noreply.github.com>
2023-05-07 10:36:51 +0600

    Add files via upload

---
+++ home_page_alt1_widget.dart
@@ -0,0 +1,97 @@
+// TODO Implement this library.
+
+import '/flutter_flow/flutter_flow_animations.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/pages/m_y_profile_page/m_y_profile_page_widget.dart';
+import '/pages/transaction_a_d_d/transaction_a_d_d_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'home_page_alt1_model.dart';
+export 'home_page_alt1_model.dart';
+
+class HomePageAlt1Widget extends StatefulWidget {
+  const HomePageAlt1Widget({Key? key}) : super(key: key);
+
+  @override
+  _HomePageAlt1WidgetState createState() => _HomePageAlt1WidgetState();
+}
+
+class _HomePageAlt1WidgetState extends State<HomePageAlt1Widget>
+    with TickerProviderStateMixin {
+  late HomePageAlt1Model _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final animationsMap = {
+    'rowOnPageLoadAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onPageLoad,
+      effects: [
+        FadeEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: 0,
+          end: 1,
+        ),
+        MoveEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: Offset(0, 22),
+          end: Offset(0, 0),
+        ),
+        ScaleEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: Offset(0.2, 0.2),
+          end: Offset(1, 1),
+        ),
+      ],
+    ),
+    'columnOnPageLoadAnimation': AnimationInfo(
+      trigger: AnimationTrigger.onPageLoad,
+      effects: [
+        FadeEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: 0,
+          end: 1,
+        ),
+        MoveEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: Offset(0, 60),
+          end: Offset(0, 0),
+        ),
+        ScaleEffect(
+          curve: Curves.easeInOut,
+          delay: 0.ms,
+          duration: 600.ms,
+          begin: Offset(1, 1),
+          end: Offset(1, 1),
+        ),
+      ],
+    ),
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => HomePageAlt1Model());
+
+    setupAnimations(
+      animationsMap.values.where((anim) =>
+          anim.trigger == AnimationTrigger.onActionTrigger ||
+          !anim.applyInitialState),
+      this,
+    );
+  }
